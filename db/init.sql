91e42d7241dfefe7459f03a28d132994f0b2956b
JosefinLindvall <lijo18zu@student.ju.se>
2020-01-29 08:41:38 +0100

    added image for about page

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,8 @@
--- CREATE TABLE accounts (
---     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
---     username VARCHAR (50) NOT NULL,
---     password VARCHAR (30) NOT NULL,
---     CONSTRAINT usernameUnique UNIQUE (username)
--- );
+CREATE TABLE accounts (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR (50) NOT NULL,
+    password VARCHAR (30) NOT NULL,
+    CONSTRAINT usernameUnique UNIQUE (username)
+);
 
--- INSERT INTO accounts (username, password) VALUES ("Alice", "abc123")
+INSERT INTO accounts (username, password) VALUES ("Alice", "abc123")
---
+++ web-application/src/dal/account-repository.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql")
+
+const db = mysql.createConnection({
+    host: "db", 
+    user: "root", 
+    password: "abc5555", 
+    database: "webAppDb"
+})
