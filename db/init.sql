46e2a16b31eb2193fa27b3a5860e0fa72e7394a9
Dennis Andersson <dennisfram@hotmail.com>
2020-01-30 08:47:23 +0100

    first work done

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,25 @@
-CREATE TABLE accounts (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    username VARCHAR (50) NOT NULL,
-    password VARCHAR (30) NOT NULL,
-    CONSTRAINT usernameUnique UNIQUE (username)
-);
+-- CREATE TABLE IF NOT EXISTS Account (
+--     firstName varchar(255),
+--     lastName varchar(255),
+--     email VARCHAR(255) PRIMARY KEY,
+--     password VARCHAR(255),
+--     birthDate DATE(255),
+--     gender VARCHAR(255),
+--     flag VARCHAR(255)
+-- );
 
-INSERT INTO accounts (username, password) VALUES ("Alice", "abc123")
+-- CREATE TABLE IF NOT EXISTS Post (
+--     email VARCHAR(255) FOREIGN KEY ON Account email,
+--     title VARCHAR(255),
+--     content VARCHAR(255),
+--     email + title PRIMARY KEY  
+-- );
+
+-- CREATE TABLE IF NOT EXISTS ContactMessage (
+--     title VARCHAR(255),
+--     content VARCHAR(255),
+--     email VARCHAR(255), 
+--     email + title PRIMARY KEY
+-- );
+
+-- INSERT INTO accounts (username, password) VALUES ("Alice", "abc123")
--- web-application/src/pl/app.js
+++ web-application/src/pl/app.js
@@ -20,6 +20,26 @@ app.get('/', function (request, response) {
     response.render("home.hbs")
 })
 
+app.get('/home', function (request, response) {
+    response.render("home.hbs")
+})
+
+app.get('/login', function(request, response) {
+    response.render("login.hbs")
+})
+
+app.get('/signup', function(request, response) {
+    response.render("signUp.hbs")
+})
+
+app.get('/create-post', function(request, response) {
+    response.render("createPost.hbs")
+})
+
+app.get('/profile', function(request, response) {
+    response.render("profile.hbs")
+})
+
 app.listen(8080, function () {
     console.log('Web application listening on port 8080')
 })
