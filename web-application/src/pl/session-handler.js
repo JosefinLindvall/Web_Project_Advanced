8edd224ca10e9e7fbcbd5a4bd440392abcf097aa
Dennis Andersson <dennisfram@hotmail.com>
2020-02-17 08:59:19 +0100

    merge complete and other stuff

---
+++ web-application/src/pl/session-handler.js
@@ -0,0 +1,41 @@
+
+
+
+exports.checkedIfLoggedInAsRegUser = function (request, response, next){
+
+    if (request.session.loggedInAsReg == null) {
+
+        const model = {
+            notLoggedInAsReg: true
+        }
+
+        response.render("login.hbs", model)
+    }
+
+    else {
+        next()
+    }
+        
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+exports.checkedIfLoggedInAsAdminUser = function (request, response, next){
+
+    if (request.session.loggedInAsAdmin == null) {
+
+        const model = {
+            notLoggedInAsAdmin: true
+        }
+
+        response.render("login.hbs", model)
+    }
+
+    else {
+        next()
+    }
+        
+
+}
