e8963626281256e7aec6eee862fa9c616dbd8edf
Dennis Andersson <dennisfram@hotmail.com>
2020-02-26 18:04:09 +0100

    now it should be correct

--- web-application/src/dal-PostgreSQL/db.js
+++ web-application/src/dal-PostgreSQL/db.js
@@ -1,18 +1,4 @@
 
-<<<<<<< HEAD
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('postgres://friendy:5555@postgres:5432/webAppPostgreSQLDb') 
-
-
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-
-=======
-const sequelize = new Sequelize('postgres://friendy:5555@postgres:5432/webAppPostgreSQLDb') 
->>>>>>> part7_branch_Dennis
+const sequelize = new Sequelize('postgres://friendy:5555@postgres:5432/webAppPostgreSQLDb')
 
 module.exports = sequelize
