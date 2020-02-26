a6f23b004b8e73534b5f75987bf17b721b70830d
Dennis Andersson <dennisfram@hotmail.com>
2020-02-26 12:13:09 +0100

    done with my part but everything is fucked up

--- web-application/src/dal-PostgreSQL/account-repository.js
+++ web-application/src/dal-PostgreSQL/account-repository.js
@@ -5,7 +5,7 @@ module.exports = function ({ }) {
     return {
 
         createAccount: function (account, hash, callback) {
-            
+
             Account.create({
                 firstName: account.firstName,
                 lastName: account.lastName,
@@ -15,63 +15,34 @@ module.exports = function ({ }) {
                 birthDate: account.birthday,
                 gender: account.gender,
                 typeOfUser: "User"
-            
-            }).then(function(account) {
+
+            }).then(function (account) {
                 Account.findOne({
-                    where: {email: account.email}
-                
-                }).then(function(account) {
+                    where: { email: account.email }
+                }).then(function (account) {
                     callback(null, account.id)
-                
                 })
-            }).catch(function(error) {
-                callback(error, null)  
+            }).catch(function (error) {
+                callback(error, null)
             })
         },
 
+        //dubbelkolla hur detta kommer bli för vi parsar ju objektet och stoppar in fler parameterar 
         logInAccount: function (typedEmail, callback) {
-
-            //TODO
-            // Account.findOne(function(){
-            //     where: {}
-        
-            // })
-
-            const query = "SELECT password, typeOfUser, accountID FROM `Account` WHERE email = ?"
-            const values = [typedEmail]
-
-            db.query(query, values, function (databaseError, dataPackage) {
-
-                databasePassword = dataPackage[0].password
-                typeOfUser = dataPackage[0].typeOfUser
-                accountID = dataPackage[0].accountID
-
-                if (databaseError) { // This does not mean that no email was found, it means that we have an actual database error
-                    callback(['A database error occured when trying to log you in.'], null, null, null)
-                }
-
-                else if (databasePassword.length > 0) { // We have a match! An email was found that matched the users email.
-                    callback(null, databasePassword, typeOfUser, accountID)
-                }
-
-                else { // No email found that matches the users email
-                    callback(['Invalid email!'], null, null, null)
-                }
+            Account.findOne({
+                where: { email: typedEmail }
+            }).then(function (dataPackage) {
+                callback(null, dataPackage)
+            }).catch(function (error) {
+                callback(error, null)
             })
         },
 
-        // TODO 
         getUserInformation: function (accountID, callback) {
-            const query = "SELECT firstName, lastName, email, phoneNumber, birthDate, gender FROM `Account` WHERE accountID = ?"
-            const values = [accountID]
-
-            db.query(query, values, function (databaseError, currUserInfo) {
-                if (databaseError) { 
-                    callback(['A database error occured when trying to get the user in.'], null)
-                }
-                else {
-                    callback(null, currUserInfo)
-                }
+            Account.findById(accountID).then(function (currUserInfo) {
+                callback(null, currUserInfo)
+            }).catch(function (error) {
+                callback(error, null)
             })
         }
     }
